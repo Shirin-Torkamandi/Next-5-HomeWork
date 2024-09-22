a865a38d011f37ecef74d734878ecc2f0be25e9a
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-22 16:14:38 +0330

    Add Form for posts, recipes & users components

---
+++ nextjs-application/src/components/Form.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { addPost } from "@/utils/actions";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+export default function FormForPost() {
+  const [title, setTitle] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await addPost(title);
+    setTitle("");
+  }
+  return (
+    <Box
+      bgcolor={"#f5f5f5"}
+      component="form"
+      onSubmit={(e) => handleSubmit(e)}
+      sx={{ width: 500, maxWidth: "100%" }}
+      marginTop={4}
+      marginLeft={4}
+    >
+      <TextField
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        label="Enter Title"
+        id="fullWidth"
+      />
+    </Box>
+  );
+}
---
+++ nextjs-application/src/components/FormForUser.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { addUser } from "@/utils/actions";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+export default function FormForUser() {
+  const [title, setTitle] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await addUser(title);
+    setTitle("");
+  }
+  return (
+    <Box
+      bgcolor={"#f5f5f5"}
+      component="form"
+      onSubmit={(e) => handleSubmit(e)}
+      sx={{ width: 500, maxWidth: "100%" }}
+      marginTop={4}
+      marginLeft={4}
+    >
+      <TextField
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        label="Enter Title"
+        id="fullWidth"
+      />
+    </Box>
+  );
+}
